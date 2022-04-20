9badc610b4d610af3197fe60f76cfa8f739bde2a
Divine <dpkreativ@gmail.com>
2022-04-20 10:12:24 +0100

    add indicator to click on blue button

--- pages/index.js
+++ pages/index.js
@@ -44,13 +44,20 @@ export default function Home() {
           className="mt-5 grid gap-5 md:grid-cols-5"
         >
           {/* Image Box: to store and preview the image before text extraction begins */}
-          <div className="w-full h-72 col-span-5 md:col-span-2">
-            {imageSrc && (
+          <div className="w-full h-72 col-span-5 md:col-span-2 flex">
+            {imageSrc ? (
               <img
                 src={imageSrc}
                 alt="uploaded image"
                 className="object-contain h-full w-full"
               />
+            ) : (
+              <span className="self-center flex flex-col space-y-3">
+                <p>Click on the blue button below to add an image</p>
+                <div className="animate-bounce">
+                  <ArrowSVG />
+                </div>
+              </span>
             )}
           </div>
 
@@ -120,6 +127,23 @@ const LoadingSVG = () => {
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       ></path>
+    </svg>
+  );
+};
+
+// ========= BOUNCING ARROW SVG COMPONENT =========
+const ArrowSVG = () => {
+  return (
+    <svg
+      className="w-6 h-6 text-blue-500"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
     </svg>
   );
 };
